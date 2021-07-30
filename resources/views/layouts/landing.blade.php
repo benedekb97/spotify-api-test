861a1fcb3d52940e1744c82e1cd1659df8dca53c
Benedek Burgess <benedekb97@gmail.com>
2021-07-30 16:35:56 +0200

    Add foundation css

---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <title>Spotify Playground</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    </head>
+    <body>
+        <div class="flex-container">
+
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
