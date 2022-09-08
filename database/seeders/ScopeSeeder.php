2f65aef9b9a490bad54f2f7eca0dbda2f0a10453
benedek-burgess <benedek.burgess@prt.manomano.com>
2022-09-08 10:21:53 +0200

    Update ScopeSeeder.php to use Doctrine

--- database/seeders/ScopeSeeder.php
+++ database/seeders/ScopeSeeder.php
@@ -4,20 +4,36 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Factories\ScopeFactoryInterface;
 use App\Http\Api\Authentication\SpotifyAuthenticationApiInterface;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Database\Seeder;
 
-// TODO: Use actual factory, not shit eloquent create method (the class has been deleted anyways lol)
 class ScopeSeeder extends Seeder
 {
+    private ScopeFactoryInterface $scopeFactory;
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        ScopeFactoryInterface $scopeFactory,
+        EntityManager $entityManager
+    ) {
+        $this->scopeFactory = $scopeFactory;
+        $this->entityManager = $entityManager;
+    }
+
     public function run(): void
     {
         foreach (SpotifyAuthenticationApiInterface::SCOPE_ENABLED_MAP as $scope => $enabled) {
-            Scope::create(
-                [
-                    'name' => $scope
-                ]
-            );
+            if ($enabled) {
+                $scopeModel = $this->scopeFactory->create($scope);
+
+                $this->entityManager->persist($scopeModel);
+            }
         }
+
+        $this->entityManager->flush();
     }
 }
